ae86f2dee93b53a10b1cd76a11a63329e441868c
Arsenii Litvinchuk <alitvinchuk346@gmail.com>
2026-05-27 20:50:30 +0300

    final for demo: fixes

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -259,7 +259,7 @@ export default function AuthScreen() {
       </Pressable>
 
       <Pressable style={s.themeToggle} onPress={toggleTheme}>
-        {isDark ? <Sun color={c.textMain} size={24} /> : <Moon color={c.textMain} size={24} />}
+        {isDark ? <Moon color={c.textMain} size={24} /> : <Sun color={c.textMain} size={24} />}
       </Pressable>
 
       <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
